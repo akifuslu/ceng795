9d3354c755ec8f38729ce47dedd2177cf38f27df
Akif Üslü <>
2021-04-08 11:25:36 +0300

    fix

--- ray.h
+++ ray.h
@@ -4,22 +4,11 @@
 
 namespace raytracer
 {
-    class Object;
     class Ray
     {
         public:
             Ray(Vector3f origin, Vector3f direction);
             Vector3f Origin;
             Vector3f Direction;
-    };
-
-    class RayHit
-    {
-        public:
-            Object* Object;
-            Material Material;
-            float T;
-            Vector3f Point;
-            Vector3f Normal;
     };
 }
